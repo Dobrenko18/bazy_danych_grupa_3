71f01eae4ca1d8f1e26e879a151ce5c4a17c26c1
Dobrenko18 <92035759+Dobrenko18@users.noreply.github.com>
2021-11-04 11:00:59 +0100

    Add files via upload

---
+++ min_max.cpp
@@ -0,0 +1,111 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void wypisz(vector<int>tab)
+{
+	for (int i = 0; i < tab.size(); i++)
+	{
+		cout << tab[i] << ",";
+	}
+	cout << "\n";
+}
+
+void wypisz2(int a)
+{
+	cout << a << "\n";
+}
+
+bool czynalezy(vector<int>tab, int element)
+{
+	for (int i = 0; i < tab.size(); i++)
+	{
+		if (tab[i] == element)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool czyzawiera(vector<int>tab,vector<int>podzbior)
+{
+	for (int i = 0; i < podzbior.size(); i++)
+	{
+		if (!czynalezy(tab,podzbior[i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int suma(int a, int b)
+{
+	return a + b;
+}
+
+int Min(vector<int>tab)
+{
+	int wynik = tab[0];
+	for (int i = 0; i < tab.size(); i++)
+	{
+		if (tab[i] < wynik)
+		{
+			wynik = tab[i];
+		}
+	}
+	return wynik;
+}
+
+int Max(vector<int>tab)
+{
+	int wynik = tab[0];
+	for (int i = 0; i < tab.size(); i++)
+	{
+		if (tab[i] > wynik)
+		{
+			wynik = tab[i];
+		}
+	}
+	return wynik;
+}
+
+int main()
+{
+	int a = 3 + 5;
+	int b = suma(a, 10);
+	wypisz2(b);
+
+	vector<int>tab = { 1,2,3,4 };
+	wypisz(tab);
+
+	cout << "Czy nalezy: ";
+	bool wynik = czynalezy(tab, 5);
+	if (wynik)
+	{
+		cout << "tak";
+	}
+	else
+	{
+		cout << "nie";
+	}
+
+	cout << "\n";
+	cout << "Czy zawiera: ";
+	vector<int>podzbior = { 1 };
+
+	bool wynik2 = czyzawiera(tab, podzbior);
+	if (wynik2)
+	{
+		cout << "tak";
+	}
+	else
+	{
+		cout << "nie";
+	}
+
+	cout <<"\n" << Min(tab);
+	cout << "\n" << Max(tab);
+}
