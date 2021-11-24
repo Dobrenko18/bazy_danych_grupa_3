73cebf4e93cc2537f04a74d3fee7c7c651032adf
Dobrenko18 <92035759+Dobrenko18@users.noreply.github.com>
2021-11-24 16:11:36 +0100

    Update lab_07.sql

--- lab_07.sql
+++ lab_07.sql
@@ -1,1 +1,18 @@
+#zadanie 1
+2.
+select * from kreatura k
+left join uczestnicy u on k.idKreatury=u.id_uczestnika
+where u.id_uczestnika is null;
+3.
+select w.nazwa, sum(e.ilosc) from wyprawa w 
+inner join uczestnicy u on w.id_wyprawy=u.id_wyprawy
+inner join ekwipunek e on u.id_uczestnika=e.idKreatury
+group by w.id_wyprawy;
 
+bardziej fajniej 
+
+select w.nazwa, sum(e.ilosc), group_concat(z.nazwa,'->',e.ilosc) from wyprawa w 
+inner join uczestnicy u on w.id_wyprawy=u.id_wyprawy
+inner join ekwipunek e on u.id_uczestnika=e.idKreatury
+inner join zasob z on e.idZasobu=z.idZasobu
+group by w.id_wyprawy;
