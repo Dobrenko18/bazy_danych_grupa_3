a42fcb8bd0c433fd6cbbd63ceddba799f3539e06
Dobrenko18 <92035759+Dobrenko18@users.noreply.github.com>
2021-11-24 15:18:26 +0100

    Update lab_06.sql

--- lab_06.sql
+++ lab_06.sql
@@ -48,3 +48,27 @@ select idKreatury from kreatura where idKreatury not in
 (select distinct idKreatury from ekwipunek
 where idKreatury is not NULL);
 
+#zadanie 4
+4.2
+select * from kreatura k
+inner join ekwipunek e on k.idKreatury=e.idKreatury
+inner join zasob z on e.idZasobu=z.idZasobu
+where z.rodzaj='jedzenie' order by k.dataUr desc limit 5;
+4.3
+select nazwa, idKreatury, nazwa, idKreatury from kreatura;
+select * from kreatura k
+inner join kreatura k2 on k2.idKreatury-k1.idKreatury=5;
+
+#zadanie 5
+1.
+select k.rodzaj, sum(e.ilosc), avg(e.ilosc*z.waga)
+from kreatura k
+inner join ekwipunek e on k.idKreatury=e.idKreatury
+inner join zasob z on e.idZasobu=z.idZasobu
+where k.rodzaj not in ('malpa','waz') and e.ilosc <30
+group by k.rodzaj having sum(e.ilosc) < 30;
+2.
+select k.nazwa, k.rodzaj, k.dataUr from kreatura k,
+(select rodzaj, min(dataUr) as min, max(dataUr) as max from kreatura 
+group by rodzaj) as pod
+where k.dataUr=pod.min or k.dataUr=pod.max and k.rodzaj=pod.rodzaj;
